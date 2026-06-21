eb65bdd492f630a3f6683033a88b2bf684c356c5
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-06-21 15:24:28 +0200

    aggiunta fetch per prendere 5 prodotti da mostrare nella home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,24 @@
 import { useEffect } from "react";
 import ProductList from "../ProductList";
 import { useState } from "react";
+import { fetchFive } from "../../utils/fetch";
 
 function Home() {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetchFive().then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => console.error("Errore nel recupero prodotti:", err));
+    }, [])
     
     return (
 
         <main className="homepage-main container-sm">
+            <div className="">
+                <ProductList products={products}/>
+            </div>
+            
         </main>
     )
 }
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -14,6 +14,22 @@ async function fetchAll() {
         console.error('Errore', error);
         return null
     }
-}
+};
 
-export { fetchAll };
+async function fetchFive() {
+    try {
+        const response = await fetch(`http://localhost:3000/products/initial`);
+
+        if (!response.ok) {
+            throw new Error('Errore di comunicazione col server');
+        }
+
+        const data = await response.json();
+        return data.result;
+    } catch (error) {
+        console.error('Errore', error);
+        return null
+    }
+};
+
+export { fetchAll, fetchFive };
