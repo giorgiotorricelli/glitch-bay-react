9732547186ba018f67950e667fbdac62b5ddbfee
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-06-23 18:30:37 +0200

    fix di stile

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -4,9 +4,13 @@ function ProductList({ products, displayed }) {
     return (
         <div className="container">
             <div className="row products-wrapper">
+                
                 {products.map((product) => (
-                    <ProductCard key={product.id} product={product} displayed={displayed}/>
+                    <div className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex g-4">
+                        <ProductCard key={product.id} product={product} displayed={displayed}/>
+                    </div>
                 ))}
+                
             </div>
         </div>
 
