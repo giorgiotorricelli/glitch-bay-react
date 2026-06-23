4f0cf08594d7726ea98afff8f1ea83739f2e4dea
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-06-23 13:43:32 +0200

    migliorata grafica card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
+import { EyeFill } from "react-bootstrap-icons";
 
 function ProductCard({ product, displayed }) {
     return (
-        <div className="col-6 col-md-4 col-lg-3 d-flex g-4"> {/* 1. d-flex sulla colonna */}
+        <div className="col-12 col-sm-6 col-md-4 col-lg-3 d-flex g-4"> {/* 1. d-flex sulla colonna */}
             <div className="cyber-card w-100"> {/* 2. w-100 (e la tua classe cyberpunk) */}
                 <div className="card-body d-flex flex-column"> {/* 3. d-flex e flex-column sul body */}
 
-                    <img src={product.img} alt="product-img" className="img-fluid mb-3" />
+                    <img src={product.img} alt="product-img" className="img-fluid mb-3 product-img" />
 
-                    <h5 className="card-title p-font">{product.name}</h5>
+                    <h5 className="card-title p-font prod-name-wrapper">{product.name}</h5>
 
-                    <div className="mt-auto">
+                    <div className="price-wrapper p-font">
                         <h3>${product.price}</h3>
                     </div>
 
@@ -19,7 +20,7 @@ function ProductCard({ product, displayed }) {
                     </div> : null}
 
                     {displayed !== 'product-detail' ? 
-                    <Link to={`/products/${product.slug}`} className="btn btn-primary mt-3 p-font">Esplora</Link>
+                    <Link to={`/products/${product.slug}`} className="btn mt-3 explore-btn"><EyeFill className="explore-eye"/></Link>
                     : null}
                     
 
